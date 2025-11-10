65fb5ce73132d2dbe24a99891aee687e8eda43e0
gabriel0vieira <gabriel.gdav@gmail.com>
2025-11-10 19:31:30 -0300

    Refactor app.py to improve structure and clarity

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ with abas[0]:
     col4.metric("Alarmes Ativos", df["alarme"].sum())
 
     st.markdown("### Distribuição dos Valores Captados")
-    fig = px.histogram(df, x="intensidade_gas", nbins=30, title="Distribuição de Intensidade dos Gases", color_discrete_sequence=["#0083B8"])
+    fig = px.histogram(df, x="raw_value", nbins=30, title="Distribuição de Intensidade dos Gases", color_discrete_sequence=["#0083B8"])
     st.plotly_chart(fig, use_container_width=True)
 
     st.markdown("""
